6404fa1b5e43ccaaead83f66bea0cf2053473f0b
ShriAbhinaya <balasubramanian1@ufl.edu>
2025-11-15 22:42:55 -0500

    Partner 2 changes made

--- test_calculator.py
+++ test_calculator.py
@@ -1,6 +1,6 @@
 # https://github.com/ShriAbhinaya/lab11-AA-SB
 #Partner 1: Andrea
-#Partner 2: Shri
+#Partner 2: Shri Abhinaya
 
 import math
 import unittest
@@ -8,12 +8,15 @@ from calculator import(add, subtract, multiply, divide, logarithm, exponent, squ
 
 class TestCalculator(unittest.TestCase):
     ######### Partner 2
-    # def test_add(self): # 3 assertions
-    #     fill in code
+    def test_add(self):
+        self.assertEqual(add(3, 4), 7)
+        self.assertEqual(add(-5, 2), -3)
+        self.assertEqual(add(0, 0), 0)
 
-    # def test_subtract(self): # 3 assertions
-    #     fill in code
-    # ##########################
+    def test_subtract(self):
+        self.assertEqual(subtract(10, 3), 7)
+        self.assertEqual(subtract(0, 5), -5)
+        self.assertEqual(subtract(-4, -6), 2)
 
     ######## Partner 1
     def test_multiply(self):
@@ -28,19 +31,18 @@ class TestCalculator(unittest.TestCase):
             divide(0,10)
 
     ######## Partner 2
-    # def test_divide_by_zero(self): # 1 assertion
-    #     # call division function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     div(0, 5)
-    #     fill in code
+    def test_divide_by_zero(self):
+        with self.assertRaises(ZeroDivisionError):
+            divide(0, 5)
 
-    # def test_logarithm(self): # 3 assertions
-    #     fill in code
+    def test_logarithm(self):
+        self.assertAlmostEqual(logarithm(10, 100), math.log(100, 10))
+        self.assertAlmostEqual(logarithm(2, 8), math.log(8, 2))
+        self.assertAlmostEqual(log(10, 100), math.log(100, 10))
 
-    # def test_log_invalid_base(self): # 1 assertion
-    #     # use same technique from test_divide_by_zero
-    #     fill in code
-    # ##########################
+    def test_log_invalid_base(self):
+        with self.assertRaises(ValueError):
+            logarithm(1, 10)
 
     ######## Partner 1#######
     def test_log_invalid_argument(self):
